8585f23e6cf9a7de3b0c683b4ee044d52072217b
Evgeniy <evgeniyalbul@gmail.com>
2019-11-02 23:25:42 +0300

    add Leak

---
+++ app/src/main/java/dagger/android/albul/ru/interesttools/LeakApp.java
@@ -0,0 +1,17 @@
+package dagger.android.albul.ru.interesttools;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class LeakApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)){
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
--- app/src/main/java/dagger/android/albul/ru/interesttools/MainActivity.java
+++ app/src/main/java/dagger/android/albul/ru/interesttools/MainActivity.java
@@ -2,13 +2,38 @@ package dagger.android.albul.ru.interesttools;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LEAK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        killMemory();
+    }
+
+    private void killMemory() {
+        new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... voids) {
+                int begin = 0;
+                int end = 200;
+                while (begin < end) {
+                    begin++;
+                    end--;
+                    Log.d(TAG, "doInBackground: " + (begin*end)/10);
+                    SystemClock.sleep(500);
+                }
+                return null;
+            }
+
+
+        }.execute();
     }
 }
